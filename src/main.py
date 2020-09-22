dcfaa877e0f1b86449b85d01f5420c291aa4dcfb
samirbenzada <samirbenzada@gmail.com>
2020-09-21 22:48:19 -0400

    update

--- src/main.py
+++ src/main.py
@@ -11,9 +11,9 @@ CORS(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# @app.route('/')
-# def hello_world():
-#     return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
+@app.route('/')
+def hello_world():
+    return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
 
 @app.route('/search', methods=['POST'])
 def searchComplex():
